77326f7cc1398662a1229d3b4d2ab40ee7e77f28
m4sango <m.s.1022.ttlove@gmail.com>
2020-02-08 22:27:55 +0900

    first commit

---
+++ src/main/kotlin/com/m4sango/sakaliveguide/SakaLiveGuideApplication.kt
@@ -0,0 +1,11 @@
+package com.m4sango.sakaliveguide
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SakaLiveGuideApplication
+
+fun main(args: Array<String>) {
+	runApplication<SakaLiveGuideApplication>(*args)
+}
---
+++ src/test/kotlin/com/m4sango/sakaliveguide/SakaLiveGuideApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.m4sango.sakaliveguide
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SakaLiveGuideApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
